e3332ff19601d26dc7c9c6c3903c8e6090d9f443
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-18 09:32:53 +0200

    Extending the Teacher class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,12 +7,6 @@ export interface Teacher {
 	[key: string]: any,
 }
 
-const teacher3: Teacher = {
-	firstName: 'John',
-	fullTimeEmployee: false,
-	lastName: 'Doe',
-	location: 'London',
-	contract: false,
-};
-
-console.log(teacher3);
+interface Directors extends Teacher {
+	numberOfReports: number
+}
