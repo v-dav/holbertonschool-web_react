8e2882cc113923ea136446bd956a09cf2c1f6549
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-18 10:00:21 +0200

    writing a class with interfaces

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -18,3 +18,32 @@ function printTeacher(firstName: string, lastName: string): string {
 interface printTeacherFunction {
 	(firstName: string, lastName: string): string
 }
+
+class StudentClass {
+	firstName: string;
+	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework(): string {
+		return 'Currently working'
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+}
+
+export interface StudentConstructor {
+	new(firstName: string, lastName: string): StudentClass;
+}
+
+export interface StudentClassInterface {
+	firstName: string,
+	lastName: string,
+	workOnHomework(): string,
+	displayName(): string
+}
