7fc26b5de003f7de48827892392757986749b791
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-19 10:05:04 +0200

    webpack entry point

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+import * as $ from 'jquery';
+
+$(function() {
+    const p1 = $('<p></p>').text('Holberton Dashboard').appendTo(document.body);
+
+    const p2 = $('<p></p>').text('Dashboard data for the students').appendTo(document.body);
+
+    const p3 = $('<p></p>').text('Copyright - Holberton School').appendTo(document.body);
+});
