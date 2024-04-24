9dc4a035da33cea7663d5497eefbd5df5439d20f
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-24 14:01:42 +0200

    messing around with jsx

--- react_jobs/src/App.jsx
+++ react_jobs/src/App.jsx
@@ -1,7 +1,19 @@
 const App = () => {
-	return (
-		<div className="text-5xl">App</div>
-	)
+	const names = ["Brad", "Mary", "Joe", "Sarah"]
+	const loggedIn = true;
+	const styles = {
+		color: 'red',
+		fontSize: '55px'
+	}
+	if (loggedIn) {
+	return <div className="text-5xl">
+		<ul>
+		{names.map((name, index) => (<li style={styles} key={index}>{ name }</li>))}
+		</ul>
+	</div>
+	} else {
+		return <div className="text-5xl">Not loggged in</div>
+	}
 }
 
 export default App
