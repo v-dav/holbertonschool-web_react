2f5e6d5b4756ef727e68801e4124dcee3c623f4c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-24 18:52:39 +0200

    reorder the comonents for the right display

--- react_intro/task_2/dashboard/src/index.js
+++ react_intro/task_2/dashboard/src/index.js
@@ -6,7 +6,7 @@ import Notifications from './Notifications';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
     <Notifications/>
+    <App />
   </React.StrictMode>
 );
