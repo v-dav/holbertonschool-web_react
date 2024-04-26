e2f2dac256ba3e9ec97cc196fb68088751af275e
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-26 14:51:54 +0200

    add useState hook

--- react_jobs/src/components/JobListing.jsx
+++ react_jobs/src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { FaMapMarker} from 'react-icons/fa'
 
 const JobListing = ({ job }) => {
 	const [showFullDescription, setShowFullDescription] = useState(false);
@@ -16,7 +17,8 @@ const JobListing = ({ job }) => {
 					    <h3 className="text-xl font-bold">{job.title}</h3>
 					  </div>
 			    
-							  <div className="mb-5">{description }</div>
+				<div className="mb-5">{description}</div>
+				<button onClick={() => setShowFullDescription((prevState) => !prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600"> { showFullDescription ? 'Less' : 'More' }</button>
 			    
 					  <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
 			    
@@ -24,7 +26,7 @@ const JobListing = ({ job }) => {
 			    
 					  <div className="flex flex-col lg:flex-row justify-between mb-4">
 					    <div className="text-orange-700 mb-3">
-					      <i className="fa-solid fa-location-dot text-lg"></i>
+					      <FaMapMarker className="inline text-lg mb-1 mr-1" />
 					      {job.location}
 					    </div>
 					    <a
