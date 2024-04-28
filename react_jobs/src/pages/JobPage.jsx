02b42c6d8a82f897a143531a28838b52723401ba
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-28 22:47:58 +0200

    add delete job method

--- react_jobs/src/pages/JobPage.jsx
+++ react_jobs/src/pages/JobPage.jsx
@@ -3,11 +3,20 @@ import { useParams, useLoaderData } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const JobPage = () => {
+const JobPage = ( {deleteJob}) => {
 	// const [job, setJob] = useState(null);
 	const { id } = useParams();
 	const job = useLoaderData();
+	const navigate = useNavigate();
+
+	const onDeleteClick = (jobId) => {
+		const confirm = window.confirm('Are you sure you want to delete this listing ?')
+		if (!confirm) return;
+		deleteJob(jobId);
+		navigate('/jobs');
+	}
 
 	// const [loading, setLoading] = useState(true)
 
@@ -100,7 +109,7 @@ const JobPage = () => {
 								  to={`/jobs/edit/${job.id}`}
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link>
-              <button
+              <button onClick={ () => onDeleteClick(job.id)}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
